8243f885644a4444b37b0b2e03ea27ffb0498cfb
matthew hallberg <matthewhallberg@gmail.com>
2018-02-18 04:43:30 -0500

    first commit

---
+++ Assets/PlaneFinder.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneFinder : MonoBehaviour {
+
+	public GameObject reticle;
+	
+	void Update()
+	{
+		Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
+		RaycastHit hit;
+		if (Physics.Raycast (ray, out hit)) {
+			if (hit.transform.CompareTag ("plane"))
+				reticle.SetActive (true);
+		} else {
+			reticle.SetActive (false);
+		}
+	}
+}
